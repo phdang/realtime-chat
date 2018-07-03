a4182cceab68190df747dffc0d0644c7a0a44c64
phdang <phdang94@yahoo..com>
2018-07-03 15:55:27 +0700

    fit round user status

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -19,6 +19,9 @@ $(function() {
           '</strong></div><div class="user-status-on"></div></li>'
       );
     }
+    var width = document.getElementsByClassName('user-status-on')[0]
+      .offsetWidth;
+    $('.user-status-on').css({ height: width + 'px' });
     var sndJoin = new Audio('/audio/filling-your-inbox.mp3'); // buffers automatically when created
     sndJoin.play();
     $('.join-room-msg').html(msg.message);
